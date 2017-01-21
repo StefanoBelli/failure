9c82346518f104d9d51ba613cd5ece4897dfd885
StefanoBelli <sa.belli@hotmail.it>
2017-01-21 10:25:13 +0100

    FAILURE_SIV

--- failure.h
+++ failure.h
@@ -10,20 +10,20 @@
 #define EXIT_FAILURE -1
 #endif
 
-#define FAILURE_EIV static inline void
+#define FAILURE_SIV static inline void
 #define MAX_BUFFER 256
 
 /* Failure handling functions, based on errno value */
 
 /* failure with default exit code */
-FAILURE_EIV perr_die(const char* err) {
+FAILURE_SIV perr_die(const char* err) {
     if(errno) {
         perror(err);
         exit(EXIT_FAILURE);
     }
 }
 
-FAILURE_EIV perrf_die(const char* fmt,...) {
+FAILURE_SIV perrf_die(const char* fmt,...) {
     if(errno) {
         char target[MAX_BUFFER];
         va_list args;
@@ -39,27 +39,27 @@ FAILURE_EIV perrf_die(const char* fmt,...) {
 
 /**/
 
-FAILURE_EIV err_die() {
+FAILURE_SIV err_die() {
     if(errno) {
         perror("Error");
         exit(EXIT_FAILURE);
     }
 }
 
-FAILURE_EIV err_die_no_output() {
+FAILURE_SIV err_die_no_output() {
     if(errno)
         exit(EXIT_FAILURE);
 }
 
 /* Failure with specified exit code */
-FAILURE_EIV perr_die_with_code(const char* err, int code) {
+FAILURE_SIV perr_die_with_code(const char* err, int code) {
     if(errno) {
         perror(err);
         exit(code);
     }
 }
 
-FAILURE_EIV perrf_die_with_code(int code, const char* fmt, ...) {
+FAILURE_SIV perrf_die_with_code(int code, const char* fmt, ...) {
     if(errno) {
         char target[MAX_BUFFER];
         va_list args;
@@ -71,14 +71,14 @@ FAILURE_EIV perrf_die_with_code(int code, const char* fmt, ...) {
     }
 }
 
-FAILURE_EIV err_die_with_code(int code) {
+FAILURE_SIV err_die_with_code(int code) {
     if(errno) {
         perror("Error");
         exit(code);
     }
 }
 
-FAILURE_EIV err_die_no_output_with_code(int code) {
+FAILURE_SIV err_die_no_output_with_code(int code) {
     if(errno)
         exit(code);
 }
